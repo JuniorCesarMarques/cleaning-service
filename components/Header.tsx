5de64396c06b54b0765b1e754a356eb6e474029b
JuniorCesarMarques <jcesargithub@gmail.com>
2025-08-21 08:55:49 -0300

    menu e navbar prontos

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,49 @@
 import { FaRegClock } from "react-icons/fa6";
 import { FaFacebookF, FaInstagram  } from "react-icons/fa";
 
+import { BsFillTelephoneFill } from "react-icons/bs";
+
 export default function Header() {
     return(
-        <div className="bg-primary font-roboto flex flex-col sm:flex-row justify-between gap-3 p-3">
-            <span className="flex justify-center items-center gap-2 font-bold text-white">
-                <FaRegClock />
-                Open : 9am - 5pm ( Sunday - Friday )
-            </span>
-            <span className="text-white flex justify-center gap-2">
-                <FaFacebookF />
-                <FaInstagram  />
-            </span>
-        </div>
+        <header>
+            <div className="bg-primary font-roboto flex flex-col sm:flex-row justify-between gap-3 p-3">
+                <span className="flex justify-center items-center gap-2 font-bold text-white">
+                    <FaRegClock />
+                    Open : 9am - 5pm ( Sunday - Friday )
+                </span>
+                <span className="text-white flex justify-center gap-2">
+                    <FaFacebookF />
+                    <FaInstagram  />
+                </span>
+            </div>
+
+            <div className="hidden lg:flex items-center justify-between px-30 py-5">
+                <img className="w-35" src="/logo.png" alt="" />
+                <div className="flex items-center gap-6">
+
+                    <div className="flex gap-2">
+                        <BsFillTelephoneFill className="text-white bg-primary rounded-full w-10 h-10 p-2" />
+                        <div className="flex flex-col items-center">
+                            <p className="font-bold text-zinc-600">Call Us Anytime</p>
+                            <p className="text-primary font-bold">(508) 665-7196</p>
+                        </div>
+                    </div>
+                    <div className="flex gap-2">
+                        <BsFillTelephoneFill className="text-white bg-primary rounded-full w-10 h-10 p-2" />
+                        <div className="flex flex-col items-center">
+                            <p className="font-bold text-zinc-600">Call Us Anytime</p>
+                            <p className="text-primary font-bold">(508) 665-7196</p>
+                        </div>
+                    </div>
+                    <div className="flex gap-2">
+                        <BsFillTelephoneFill className="text-white bg-primary rounded-full w-10 h-10 p-2" />
+                        <div className="flex flex-col items-center">
+                            <p className="font-bold text-zinc-600">Call Us Anytime</p>
+                            <p className="text-primary font-bold">(508) 665-7196</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,26 +14,43 @@ export default function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="p-4 flex items-center justify-between">
-      <Popover>
-        <PopoverTrigger asChild>
-          <button onClick={() => setIsOpen(!isOpen)} className="w-10 h-10 flex justify-center items-center bg-primary rounded text-white">
-            {!isOpen ? <IoIosMenu size={28} /> : <span className="text-2xl font-bold">X</span>}
-          </button>
-        </PopoverTrigger>
-        <PopoverContent className="w-64">
-          <ul>
-            <li>HOME</li>
-            <li>SERVICES</li>
-            <li>ABOUT US</li>
-            <li>CONTACT US</li>
-          </ul>
-        </PopoverContent>
-      </Popover>
+    <nav className="flex items-center justify-between">
+        {/* Desktop menu */}
+        <div className="hidden lg:flex items-center justify-between w-full p-5">
+            <ul className="flex gap-2 text-primary font-bold uppercase">
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Contact us</li>
+            </ul>
+            <button className="uppercase bg-primary text-white px-6 py-2 rounded font-bold">
+                 Contact us
+             </button>
+        </div>
+        {/* Mobile menu */}
+      <div className="lg:hidden flex justify-between w-full p-4">
+          <Popover>
+            <PopoverTrigger asChild>
+              <button onClick={() => setIsOpen(!isOpen)} className="w-10 h-10 flex justify-center items-center bg-primary rounded text-white">
+                {!isOpen ? <IoIosMenu size={28} /> : <span className="text-2xl font-bold">X</span>}
+              </button>
+            </PopoverTrigger>
+            <PopoverContent className="w-64">
+              <ul>
+                <li>HOME</li>
+                <li>SERVICES</li>
+                <li>ABOUT US</li>
+                <li>CONTACT US</li>
+              </ul>
+            </PopoverContent>
+          </Popover>
 
       <button className="uppercase bg-primary text-white px-6 py-2 rounded font-bold">
         Contact us
       </button>
+    </div>
+
+
     </nav>
   )
 }
