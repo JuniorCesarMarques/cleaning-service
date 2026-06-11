c5536497218ba3121c43f0836f0eab9fde3ae806
JuniorCesarMarques <jcesargithub@gmail.com>
2026-06-10 22:30:07 -0300

    remoção do prefixo no numero no footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -66,7 +66,7 @@ export default function Footer() {
             </div>
             <div className="text-sm">
               <p className="font-semibold">Need more help?</p>
-              <p>+1 (781) 568-1771</p>
+              <p>(781) 568-1771</p>
             </div>
           </div>
         </div>
